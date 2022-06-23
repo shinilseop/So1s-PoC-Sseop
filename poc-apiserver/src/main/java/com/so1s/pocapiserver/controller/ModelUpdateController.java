63e0e82fe919fc33cff472ca85805008ea3d2928
신일섭 <shinilseop12@gmail.com>
2022-06-23 13:17:35 +0900

    others

--- poc-apiserver/src/main/java/com/so1s/pocapiserver/controller/ModelUpdateController.java
+++ poc-apiserver/src/main/java/com/so1s/pocapiserver/controller/ModelUpdateController.java
@@ -1,6 +1,7 @@
 package com.so1s.pocapiserver.controller;
 
 import com.so1s.pocapiserver.service.BuildService;
+import com.so1s.pocapiserver.service.KubernetesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,28 +11,36 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 
 @Controller
-public class BuildController {
+public class ModelUpdateController {
 
     private BuildService buildService;
+    private KubernetesService kubernetesService;
 
     @Autowired
-    public BuildController(BuildService buildService) {
+    public ModelUpdateController(BuildService buildService, KubernetesService kubernetesService) {
         this.buildService = buildService;
+        this.kubernetesService = kubernetesService;
     }
 
     @PostMapping("/upload")
-    public String saveFile(@RequestParam String itemName, @RequestParam MultipartFile attachFile) throws IOException {
-        if(!buildService.saveFile(itemName, attachFile)) {
+    public String saveFile(@RequestParam MultipartFile attachFile) {
+        System.out.print("SAVE Model...");
+        if(!buildService.saveFile(attachFile)) {
             return "/uploadFailed";
         }
+        System.out.println(" Finish!");
 
-        if(!buildService.buildImage()){
+        System.out.print("Build Job...");
+        if(!kubernetesService.buildImage()){
             return "/buildFailed";
         }
+        System.out.println(" Finish!");
 
-        if(!buildService.updateInferenceServer()){
+        System.out.print("Deploy New Inference...");
+        if(!kubernetesService.updateInferenceServer()){
             return "/updateFailed";
         }
+        System.out.println(" Finish!");
 
         return "/Success";
     }
